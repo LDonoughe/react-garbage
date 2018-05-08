aa966d47cc59d1aec82b020db39623137d308d41
Logan Donoughe <LDonoughe@grio.com>
2018-05-08 12:52:21 -0700

    Adds ability to take out the trash

--- src/App.js
+++ src/App.js
@@ -7,12 +7,19 @@ class App extends Component {
     super(props);
     this.state = {garbage: []}
     this.addGarbage = this.addGarbage.bind(this)
+    this.removeTrash = this.removeTrash.bind(this)
   }
 
   addGarbage() {
-    alert('🗑️ haha you\'ve been totally trashed by React Garbage! 🗑️')
+//    alert('🗑️ haha you\'ve been totally trashed by React Garbage! 🗑️')
     this.setState({
-      garbage: this.state.garbage + ['trash']
+      garbage: [...this.state.garbage, 'trash']
+    })
+  }
+
+  removeTrash() {
+    this.setState({
+      garbage: []
     })
   }
   
@@ -22,11 +29,16 @@ class App extends Component {
         Welcome to React Garbage
       <br />
       <GenerateTrashButton addGarbage={this.addGarbage}/>
+      <div className="Trash">
+        {this.state.garbage.map(garbage => <div>{garbage}</div>)}
+      </div>
+      <RemoveTrashButton removeTrash={this.removeTrash}/>
       </div>
     );
   }
 }
 
 const GenerateTrashButton = ({addGarbage}) => <button onClick={addGarbage}>Generate Trash</button> 
+const RemoveTrashButton = ({removeTrash}) => <button onClick={removeTrash}>Take out the Trash!</button> 
 
 export default App;
